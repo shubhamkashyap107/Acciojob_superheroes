005933a9a11809fcf43c6f742934981462d89c69
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-09-17 16:47:32 +0530

    Commit

---
+++ Day4/if-else/app.js
@@ -0,0 +1,148 @@
+// let gender = "female"
+
+// if(gender == "male")
+// {
+//     console.log("Boy")
+// }
+// else
+// {
+//     console.log("Girl")
+// }
+
+
+
+
+// let age = 18;
+
+// if(age < 18)
+// {
+//     console.log("restricted content, please go back")
+// }
+// else if(age > 40){
+//     console.log("Youre too old for this website")
+// }
+// else if(age >= 18){
+//     console.log("access granted")
+// }
+// let age = 22;
+// let isStudent = false;
+// if(age >= 18)
+// {
+//     if(isStudent == true)
+//     {
+//         console.log("You are eligible")
+//     }
+//     else
+//     {
+//         console.log("You're not eligible for this offer")
+      
+//     }
+// }
+// else
+// {
+//     console.log("You're not eligible for this offer")
+// }
+
+// positive - negative
+
+// let number = Number(prompt("Enter the number to be checked"))
+
+
+// if(number < 0)
+// {
+//     alert("Negative")
+// }
+
+// else if(number > 0){
+//     alert("Positive")
+// }
+// else if(number === 0)
+// {
+//     alert("Zero")
+// }
+
+
+//odd - even
+
+// let number = prompt("Enter your number")
+
+// if(number == 0)
+// {
+//     alert("zero")
+// }
+// else if(number % 2 == 0)
+// {
+//     alert("even")
+// }
+// else{
+//     alert("Odd")
+// }
+// else if(number % 2 != 0)
+// {
+//     alert("odd")
+// }
+
+
+let n1 = Number(prompt("Enter number 1"))
+let n2 = Number(prompt("Enter number 2"))
+let n3 = Number(prompt("Enter number 3"))
+let n4 = Number(prompt("Enter number 4"))
+let n5 = Number(prompt("Enter number 5"))
+
+let avg = (n1 + n2 + n3 + n4 + n5) / 5
+
+if(avg > 90)
+{
+    alert("A+")
+}
+else if(avg > 80)
+{
+    alert("A")
+}
+else if(avg > 70)
+{
+    alert("B+")
+}
+else if(avg >= 60)
+{
+    alert("B")
+}
+else
+{
+    alert("Fail")
+}
+
+// if(avg >= 91 && avg <= 100)
+// {
+//     alert("A+")
+// }
+// else if(avg >= 81 && avg <= 90)
+// {
+//     alert("A")
+// }
+// else if(avg >= 71 && avg <= 80)
+// {
+//     alert("B+")
+// }
+// else if(avg >= 61 && avg <= 70)
+// {
+//     alert("B")
+// }
+// else{
+//     alert("Fail")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
