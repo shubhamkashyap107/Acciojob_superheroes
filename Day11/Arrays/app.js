21b79a6b764e35b5638bd64ac96066b9df68a646
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-09-30 17:30:36 +0530

    COmmit

---
+++ Day11/Arrays/app.js
@@ -0,0 +1,149 @@
+// let arr = []
+// console.log(arr)
+
+
+// arr = [1,2,3,4,5]
+// console.log(arr)
+
+
+// let arr = []
+// arr = [1,2,3,4,5]
+
+
+
+// int[]arr = new int[]
+// let arr = [1, "two", false, function(){console.log("HEHE")}, {}, [11,22,33,44,[false, "ans"]]]
+// console.log(arr)
+// console.log(arr[1])
+// console.log(arr[0])
+// console.log(arr[2])
+// console.log(arr[3]())
+// console.log(arr[5][4][1])
+
+
+
+// let example = [1,2,[3,4], [5,6], [7,8]]
+// let example = [1,2,[3,4, [5,6, [7,8]]]]
+// let example = [1,2,[function(){console.log("OKAY")}]]
+// let arr = example.flat(2)
+// let arr = example.flat()
+// console.log(arr)
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+// console.log(arr[5])
+// console.log(example.length)
+
+
+
+
+
+// let arr = []
+// arr[0] = "SHubham"
+// console.log(arr)
+// arr[5] = "gabbar"
+// console.log(arr)
+// arr[0] = "Bheem"
+// console.log(arr)
+
+// arr.push(1)
+// arr.push(1)
+// arr.push(1)
+// arr.push(1)
+// arr.push(1)
+// arr.push(1)
+// let ans = arr.push(1)
+// console.log(arr, ans)
+
+// arr.push(6,7,8,9,10)
+// console.log(arr, ans)
+
+// arr.push([1,2,3,4,5,6,7,8])
+// console.log(arr)
+
+
+// let arr = [1,2,3,4,5]
+// let retVal = arr.pop()
+
+// console.log(arr, retVal)
+// arr.pop()
+// console.log(arr)
+// arr.pop()
+// arr.pop()
+// arr.pop()
+// console.log(arr)
+// let val = arr.pop()
+// console.log(val)
+
+
+
+
+
+// let arr = [1,2,3,4,5, ["a", "b", "c", ["ans"]]]
+
+// let a1 = arr.pop()
+// console.log(a1)
+
+// let a2 = a1.pop()
+// console.log(a2)
+
+
+// let ans = a2.pop()
+// console.log(ans)
+
+
+
+
+
+
+// let arr = [1,2,3,4,5]
+// console.log(arr)
+// arr.unshift(0)
+// console.log(arr)
+
+
+
+// arr.shift()
+// console.log(arr)
+
+
+// let arr = []
+// arr.unshift(1)
+// arr.unshift(2)
+// arr.unshift(3)
+// arr.unshift(4)
+// arr.unshift(5)
+// console.log(arr)
+
+// console.log(arr.shift())
+
+
+
+// let a1 = [1,2,3,4,5]
+// let a2 = ["a","b","c","d","e"]
+// let a3 = [true, false, true, false, true, false]
+
+
+// let arr = a1.concat(a2)
+// let arr = a2.concat(a1).concat(a3).concat(function(){}, function(){})
+
+// console.log(arr)
+// console.log(a1 + a2)
+
+
+
+// let arr = [12,34,12,45,67,879,23,65]
+
+// for(let i = 0; i < arr.length; i++)
+// {
+//     console.log(arr[i])
+// }
+
+
+
+let arr = []
+
+// n -> size of array
+// n elements
