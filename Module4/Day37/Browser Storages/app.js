747e5cfb0221aff8dea40b847acf13ff836927bb
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-11-26 16:39:50 +0530

    Commit

---
+++ Module4/Day37/Browser Storages/app.js
@@ -0,0 +1,29 @@
+// localStorage.setItem("name", "Modiji")
+
+// let value = localStorage.getItem("name")
+// console.log(value)
+
+// localStorage.setItem("name2", "nitish ji")
+
+// localStorage.removeItem("name")
+
+
+// localStorage.clear()
+
+
+// sessionStorage.setItem("name", "Modiji")
+
+// let value = sessionStorage.getItem("name")
+// console.log(value)
+
+// sessionStorage.setItem("name2", "nitish ji")
+
+// sessionStorage.removeItem("name")
+
+
+// sessionStorage.clear()
+
+
+document.cookie = "cartoon=ishdbflibsd"
+
+document.cookie = "cartoon=; expires=Thu, 01 Jan 1970 00:00:00 UTC;"
--- Module4/Day37/Dp/app.js
+++ Module4/Day37/Dp/app.js
@@ -1,28 +1,87 @@
-class Model{
-    constructor(data){
-        this.data = data
-    }
-}
-class View{
-    render(data){
-        console.log(data)
-    }
-}
-class MVC{
-    constructor(m, v){
-        this.model = m
-        this.view = v
-    }
-    showData(){
-        this.view.render(this.model.data)
-    }
-}
-let model = new Model("This is some secret data")
-let view = new View()
-let controller = new MVC(model, view)
-controller.showData()
+// class Model{
+//     constructor(data){
+//         this.data = data
+//     }
+// }
+// class View{
+//     render(data){
+//         console.log(data)
+//     }
+// }
+// class MVC{
+//     constructor(m, v){
+//         this.model = m
+//         this.view = v
+//     }
+//     showData(){
+//         this.view.render(this.model.data)
+//     }
+// }
+// let model = new Model("This is some secret data")
+// let view = new View()
+// let controller = new MVC(model, view)
+// controller.showData()
 
 
 // console.log(model)
 // console.log(view)
 // console.log(controller)
+
+
+// class Singleton{
+
+//     static instance = false
+
+//     constructor(){
+//         if(Singleton.instance)
+//         {
+//             return Singleton.instance
+//         }
+//         Singleton.instance = this
+//         this.num = Math.random()
+//     }
+// }
+
+// let s1 = new Singleton()
+// let s2 = new Singleton()
+// let s3 = new Singleton()
+
+// console.log(s1)
+// console.log(s2)
+// console.log(s3)
+
+
+class Car{
+    constructor(brand)
+    {
+        this.brand = brand
+    }
+}
+
+
+
+class CarFactory{
+    constructor(b)
+    {
+        return new Car(b)
+    }
+}
+
+let newCar = new CarFactory("BMW")
+console.log(newCar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
