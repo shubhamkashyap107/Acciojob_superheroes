c70b814606dade5027108e40d3bb26be24118b5a
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-12-24 15:41:23 +0530

    Commit

---
+++ React/Day9/myApp/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Products from './Components/Products'
+import Loader from './Components/Loader'
+
+const App = () => {
+  return (
+    <div>
+      <Products />
+      {/* <Loader /> */}
+    </div>
+  )
+}
+
+export default App
---
+++ React/Day9/myApp/src/Components/Products.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+import Loader from './Loader'
+
+const Products = () => {
+
+    const[data, setData] = useState([])
+    const[searchQuery, setSearchQuery] = useState("")
+    const[displayData, setDisplayData] = useState([])
+
+    useEffect(() => {
+        async function getData()
+        {
+            let res = await fetch("https://dummyjson.com/products")
+            let d = await res.json()
+            console.log(d)
+            setData(d.products)
+            setDisplayData(d.products)
+        }
+
+        getData()
+    }, [])
+
+
+  return (
+    <div >
+
+
+        <input type="text" className='w-6/12 m-2 border' onChange={(e) => {
+            // setSearchQuery(e.target.value)
+
+
+            let filteredData = data.filter((item) => {
+                return item.title.toLowerCase().includes(e.target.value.toLowerCase())
+            })
+
+            // console.log(filteredData)
+            setDisplayData(filteredData)
+
+        }} />
+
+
+        <div className='grid grid-cols-4'>
+
+        {
+            displayData && displayData.map((item) => {
+                return (
+                    <div className='border p-2 rounded-lg'>
+                        <img className='w-[300px]' src={item.thumbnail} alt="" />
+
+
+                        <h3>{item.title}</h3>
+                        <p className='w-[250px]'>{item.description}</p>
+                        <p>${item.price}</p>
+                    </div>
+                )
+            })
+        }
+
+</div>
+
+    </div>
+  )
+}
+
+export default Products
